2137dd7ae5ca6592093cc6cc721507129657f194
prajwaldots <prajwal.s2294@gmail.com>
2025-11-02 22:13:01 +0530

    2-11-2025

--- 10-LocalStorage/src/App.jsx
+++ 10-LocalStorage/src/App.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const App = () => {
   localStorage.setItem('User','prajwal')  // this storages the data locally even if i close the computer this data will be stored 
    localStorage.setItem('age',18) 
-   localStorage.removeItem('age') //removes the item from local storage
+   //removes the item from local storage
    sessionStorage.setItem('random','Storage') // this is delted as soon as the browser is closed acts like temporary s
   const  User = localStorage.getItem('User')
   const Age = localStorage.getItem('age')   
@@ -13,7 +13,7 @@ const App = () => {
     city:"sedam"
   }     
   // localStorage.clear() is used to clear the local storage
- const user =  localStorage.setItem('user',JSON.stringify(User))  //json.stringify is used to convert the object into string cause object is not readable inform of pair   
+ const user =  localStorage.setItem('user',JSON.stringify(user2))  //json.stringify is used to convert the object into string cause object is not readable inform of pair   
  const getUser = localStorage.getItem(JSON.parse(user)) // JSON.parse is used too convert the stringed Object into normal Object
   return (
     <div>
